f085973814a9c7a9de58fe4b9f0778a56891389f
rrichardson2 <rrichardson2@S01PPL023627>
2022-06-23 14:46:13 -0400

    Add project files.

---
+++ Clock2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Clock2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(249, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 365);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 55);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "00/00/0000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(465, 365);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(279, 55);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Wednesday";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Clock2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Clock2
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = DateTime.Now.ToString("hh:mm:ss");
+            label2.Text = DateTime.Now.ToString("d");
+            label3.Text = DateTime.Now.ToString("dddd");
+            //Current();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = DateTime.Now.ToString("d");
+            label3.Text = DateTime.Now.ToString("dddd");
+            switch (i)
+            {
+                case int n when (n < 20)://Current time
+                    label1.Text = DateTime.Now.ToString("hh:mm:ss");
+                    break;
+
+                case int n when (n < 30)://Time to end of day
+                    DateTime end = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 20, 00, 0);
+                    TimeSpan timeLeft = end - DateTime.Now;
+                    if (timeLeft.Seconds < 0)
+                    {
+                        label1.Text = "End Of Day";
+                    }
+                    else
+                    {
+                        label1.Text = (timeLeft.ToString(@"hh\:mm\:ss") + System.Environment.NewLine + " to go");
+                    }
+                    break;
+
+                case int n when (n < 40)://Time to end of week
+                    DateTime today = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 20, 0, 0);
+                    DateTime endOfWeek = today.AddDays((DayOfWeek.Friday - today.DayOfWeek + 7) % 7);
+                    TimeSpan timeleft = endOfWeek - DateTime.Now;
+
+                    if (timeleft.Seconds < 0)
+                    {
+                        label1.Text = "End Of Week";
+                    }
+                    else
+                    {
+                        label1.Text = (timeleft.Days + " days and " + timeleft.ToString(@"hh\:mm\:ss") + " to go");
+                    }
+                    break;
+
+                case int n when (n < 50)://Time to next Holiday
+                    break;
+
+                case int n when (n >= 50):
+                    label1.Text = DateTime.Now.ToString("hh:mm:ss");
+                    i = 0;
+                    break;
+            }
+
+            i++;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
